1eb8a013a397875d0af1d1e1bdf356c826c08d02
Felix Yeung <19355619+imfelixyeung@users.noreply.github.com>
2026-09-11 19:41:17 +0100

    feat: increase max ramp duration

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,4 @@ export const RAMP_DURATION_KEY = "rampDuration";
 
 export const DEFAULT_RAMP_DURATION = 5;
 export const MIN_RAMP_DURATION = 0.5;
-export const MAX_RAMP_DURATION = 30;
+export const MAX_RAMP_DURATION = 300;
